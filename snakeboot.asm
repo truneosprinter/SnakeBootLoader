cc2b939a41316375067d65b3ba13813da513c270
Truneo Sprinter <truneosprinter@proton.me>
2024-05-27 19:52:04 +0100

    Fixed boundries and general playability issues
    Changes to be committed:
    	modified:   snakeboot.asm

--- snakeboot.asm
+++ snakeboot.asm
@@ -18,16 +18,17 @@ left_arrow = 0x4b                 ; Scan code for left arrow key
 right_arrow = 0x4d                ; Scan code for right arrow key
 down_arrow = 0x50                 ; Scan code for down arrow key
 up_arrow = 0x48                   ; Scan code for up arrow key
-    
-call clear_screen                 ; Clears 
 
+screen_width = 80                 ; Screen width in characters
+screen_height = 25                ; Screen height in characters
+
+call clear_screen                 ; Clear screen
 mov bx, hello_msg
 call print_string
 
 call handle_food                  ; Initialize and draw the food position
 
 start:                            ; Start of main loop
-
 mov ah, wait_service              ; Set wait service function
 mov cx, 1                         ; CX = 1 (wait duration high byte)
 mov dx, 0                         ; DX = 0 (wait duration low byte)
@@ -41,10 +42,11 @@ mov cx, 1                         ; Number of times to write character (1)
 mov al, ' '                       ; Character to write (space, to clear previous position)
 int video                         ; Call BIOS video interrupt to write character
 
-mov al, [food_pos]                ; Load food position into AL
-cmp [pos_row], al                 ; Compare row position with food position
+mov al, [food_row]                ; Load food row position into AL
+cmp [pos_row], al                 ; Compare row position with food row position
 jne regular_flow                  ; If not equal, continue normal flow
-cmp [pos_col], al                 ; Compare column position with food position
+mov al, [food_col]                ; Load food column position into AL
+cmp [pos_col], al                 ; Compare column position with food column position
 jne regular_flow                  ; If not equal, continue normal flow
 call handle_food                  ; If equal, handle new food position
 
@@ -65,42 +67,57 @@ int video                         ; Call BIOS video interrupt to write character
 cmp byte [scan_code], left_arrow  ; Check if left arrow key was pressed
 jne check_right_arrow             ; If not, check right arrow
 dec byte [pos_col]                ; Decrement column position (move left)
+cmp byte [pos_col], 1             ; Check if position is less than 1
+jge check_wrap_row                ; If not, continue
+mov byte [pos_col], screen_width-2; Wrap around to the right side
 jmp start                         ; Jump back to start
 
 check_right_arrow:
 cmp byte [scan_code], right_arrow ; Check if right arrow key was pressed
 jne check_up_arrow                ; If not, check up arrow
 inc byte [pos_col]                ; Increment column position (move right)
+cmp byte [pos_col], screen_width-2; Check if position is greater than or equal to screen width-1
+jl check_wrap_row                 ; If not, continue
+mov byte [pos_col], 1             ; Wrap around to the left side
 jmp start                         ; Jump back to start
 
 check_up_arrow:
 cmp byte [scan_code], up_arrow    ; Check if up arrow key was pressed
 jne check_down_arrow              ; If not, check down arrow
 dec byte [pos_row]                ; Decrement row position (move up)
+cmp byte [pos_row], 1             ; Check if position is less than 1
+jge check_wrap_row                ; If not, continue
+mov byte [pos_row], screen_height-2 ; Wrap around to the bottom
 jmp start                         ; Jump back to start
 
 check_down_arrow:
 cmp byte [scan_code], down_arrow  ; Check if down arrow key was pressed
 jne failure                       ; If not, jump to failure (invalid key)
 inc byte [pos_row]                ; Increment row position (move down)
+cmp byte [pos_row], screen_height-2 ; Check if position is greater than or equal to screen height-1
+jl check_wrap_row                 ; If not, continue
+mov byte [pos_row], 1             ; Wrap around to the top
 
+jmp start                         ; Jump back to start
+
+check_wrap_row:                   ; Check for row wrap-around
 jmp start                         ; Jump back to start
 
 failure:
 jmp $                             ; Infinite loop (do nothing)
 
 clear_screen:                     ; Subroutine to clear the screen
-mov ah, 0x02                      ; Set cursor position funciton
+mov ah, 0x02                      ; Set cursor position function
 mov bh, 0                         ; Page number (0)
 mov dh, 0                         ; Set row to 0 (start at top-left)
 mov dl, 0                         ; Set column to 0 (start at top-left part 2)
 int video                         ; Call BIOS video interrupt to set cursor position
 
-mov ah, write_char                ; Set write charachter function
+mov ah, write_char                ; Set write character function
 mov bh, 0                         ; Page number (0)
-mov al, ' '                       ; Charachter to write (space, to clear screen)
-mov cx, 2000                      ; Number of times to write charachter (80 columns * 25 rows)
-int video                         ; Call BIOS video interrupt to write charachter
+mov al, ' '                       ; Character to write (space, to clear screen)
+mov cx, 2000                      ; Number of times to write character (80 columns * 25 rows)
+int video                         ; Call BIOS video interrupt to write character
 ret                               ; Return from subroutine
 
 print_char:
@@ -132,32 +149,45 @@ end_of_handle_keyboard:
 ret                               ; Return from subroutine
 
 handle_food:                      ; Subroutine to handle food placement
-mov ah, read_time_counter         ; Set read time counter function
-int timer_int                     ; Call BIOS timer interrupt
-mov al, 7                         ; Set AL to 7 (mask for random position)
-and al, dl                        ; Perform AND operation with DL (timer value)
-mov byte [food_pos], al           ; Store result in food_pos
-add byte [food_pos], 7            ; Add 7 to ensure it's within screen boundaries
+    mov ah, read_time_counter     ; Set read time counter function
+    int timer_int                 ; Call BIOS timer interrupt
+    mov al, dl                    ; Copy timer low byte to AL
 
-mov ah, set_cursor_pos            ; Set cursor position function
-mov dh, [food_pos]                ; Load food row position into DH
-mov dl, [food_pos]                ; Load food column position into DL
-mov bh, 0                         ; Page number (0)
-int video                         ; Call BIOS video interrupt to set cursor position
+    ; Calculate row position for food
+    xor ah, ah                    ; Clear AH
+    mov cl, screen_height-4       ; Set CL to screen height - 4
+    div cl                        ; Divide AX by screen height - 4
+    add ah, 2                     ; Ensure it's within boundary (2 to screen_height - 3)
+    mov [food_row], ah            ; Store result in food_row
 
-mov ah, write_char                ; Set write character function
-mov bh, 0                         ; Page number (0)
-mov cx, 1                         ; Number of times to write character (1)
-mov al, '&'                       ; Character to write (ampersand, to represent food)
-int video                         ; Call BIOS video interrupt to write character
+    ; Calculate column position for food
+    mov al, dl                    ; Copy timer low byte to AL
+    xor ah, ah                    ; Clear AH
+    mov cl, screen_width-4        ; Set CL to screen width - 4
+    div cl                        ; Divide AX by screen width - 4
+    add ah, 2                     ; Ensure it's within boundary (2 to screen_width - 3)
+    mov [food_col], ah            ; Store result in food_col
 
-mov ah, set_cursor_pos            ; Set cursor position function
-mov dh, 0                         ; Set row to 0 (return cursor to top-left)
-mov dl, 0                         ; Set column to 0 (return cursor to top-left)
-mov bh, 0                         ; Page number (0)
-int video                         ; Call BIOS video interrupt to set cursor position
+    mov ah, set_cursor_pos        ; Set cursor position function
+    mov dh, [food_row]            ; Load food row position into DH
+    mov dl, [food_col]            ; Load food column position into DL
+    mov bh, 0                     ; Page number (0)
+    int video                     ; Call BIOS video interrupt to set cursor position
 
-ret                               ; Return from subroutine
+    mov ah, write_char            ; Set write character function
+    mov bh, 0                     ; Page number (0)
+    mov cx, 1                     ; Number of times to write character (1)
+    mov al, '&'                   ; Character to write (ampersand, to represent food)
+    int video                     ; Call BIOS video interrupt to write character
+
+    mov ah, set_cursor_pos        ; Set cursor position function
+    mov dh, 0                     ; Set row to 0 (return cursor to top-left)
+    mov dl, 0                     ; Set column to 0 (return cursor to top-left)
+    mov bh, 0                     ; Page number (0)
+    int video                     ; Call BIOS video interrupt to set cursor position
+
+    ret                           ; Return from subroutine
+
 
 pos_row:
 db 10                             ; Initial row position for player
@@ -165,11 +195,12 @@ pos_col:
 db 5                              ; Initial column position for player
 scan_code:
 db left_arrow                     ; Initial scan code (left arrow)
-food_pos:
-db 15                             ; Initial food position
+food_row:
+db 15                             ; Initial food row position
+food_col:
+db 15                             ; Initial food column position
 hello_msg:
 db ' Snake Boot', 0
-
 
 times 510 - ($ - $$) db 0         ; Fill remaining bytes with zeros until 510 bytes total
 dw 0xAA55                         ; Boot sector signature (0xAA55)
